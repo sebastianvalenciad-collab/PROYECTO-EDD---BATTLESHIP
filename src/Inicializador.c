083e8c211a173e705341fc120e984ab16823c80d
sbvastn_ <sebastian.stumpf.m@mail.pucv.cl>
2026-06-24 22:45:00 -0400

    Funcion para inicializar al jugador

---
+++ src/Inicializador.c
@@ -0,0 +1,35 @@
+void inicializar_jugador(TipoJugador *jugador, int id_asignado) {
+    if (!jugador){
+        manejar_error(ERR_PUNTERO_NULO, "guardar_turno verificacion inicial fallida."); return;
+    }
+
+    jugador->id = id_asignado;
+    jugador->vidaTotal = 0;
+
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            jugador->tablero.matriz[i][j] = 0; 
+        }
+    }
+
+    int vidas_base[MAX_BARCOS] = {5, 4, 3, 2, 2}; 
+
+    for (int i = 0; i < MAX_BARCOS; i++) {
+        jugador->barcos[i].vida = vidas_base[i];
+        jugador->vidaTotal += jugador->barcos[i].vida;
+
+        jugador->barcos[i].posX = -1;
+        jugador->barcos[i].posY = -1;
+        jugador->barcos[i].ancho = 0;
+        jugador->barcos[i].largo = 0;
+    }
+}
+void iniciar_partida(TipoJugador *usuario, TipoJugador *bot) {
+    printf("Inicializando sistemas...\n");
+
+    inicializar_jugador(usuario, 1);
+
+    inicializar_jugador(bot, 2);
+
+    printf("¡Partida inicializada! Jugadores listos para anclar la flota.\n");
+}
